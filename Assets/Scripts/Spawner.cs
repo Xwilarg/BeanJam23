f2f380c3ef048a49c77ab220d27c943cfcbae2f7
Xwilarg <xwilarg@yahoo.fr>
2022-01-23 16:52:24 +0100

    Add floor

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,21 +6,13 @@ public class Spawner : MonoBehaviour
 {
     public bool isLeft = false;
     public GameObject Bird;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void Spawn()
     {
-        GameObject bird = Instantiate(Bird, transform.position, transform.rotation);
-        bird.GetComponent<BirdIA>().isLeft = isLeft;
+        if (transform.position.y > 10f)
+        {
+            GameObject bird = Instantiate(Bird, transform.position, transform.rotation);
+            bird.GetComponent<BirdIA>().isLeft = isLeft;
+        }
     }
 }
