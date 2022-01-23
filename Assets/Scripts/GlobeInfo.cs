2828a0a1184917a0c0753c9572ed0120a68bb207
Xwilarg <xwilarg@yahoo.fr>
2022-01-23 16:27:18 +0100

    Improve health display

--- Assets/Scripts/GlobeInfo.cs
+++ Assets/Scripts/GlobeInfo.cs
@@ -1,29 +1,40 @@
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GlobeInfo : MonoBehaviour
 {
+    [SerializeField]
+    private int _lives;
 
-    public float Live;
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private GameObject _healthPrefab;
+
+    [SerializeField]
+    private Transform _canvas;
+
+    private List<GameObject> _hearts = new();
+
+    private void Start()
     {
-        
+        int size = 32;
+        for (int i = 0; i < _lives; i++)
+        {
+            var go = Instantiate(_healthPrefab, _canvas);
+            go.transform.position += Vector3.right * size * i;
+            _hearts.Add(go);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ApplyDamage()
     {
-        
-    }
-
-    public void AplyDamage(float d)
-    {
-        Live -= d;
-        if (Live <= 0)
+        Destroy(_hearts.Last());
+        _hearts.RemoveAt(_hearts.Count - 1);
+        if (!_hearts.Any())
         {
             Debug.Log("GameOver");
         }
     }
+
+    public bool IsAlive => _hearts.Any();
 }
